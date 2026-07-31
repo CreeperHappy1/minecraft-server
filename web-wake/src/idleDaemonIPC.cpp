39c01ca2839fea13de0919abf77e8b5d27834f58
CreeperHappy1 <jorgejuan.azpeitia.pozo@gmail.com>
2026-07-31 03:02:21 +0200

    most of idleDaemonIPC

--- web-wake/src/idleDaemonIPC.cpp
+++ web-wake/src/idleDaemonIPC.cpp
@@ -1,29 +1,98 @@
 #include "idleDaemonIPC.hpp"
 
-idleDaemonIPC::idleDaemonIPC(const char* cmd_fifo_path, const char* status_fifo_path){
+idleDaemonIPC::idleDaemonIPC(const char* cmd_fifo_path, const char* status_fifo_path, httplib::Server* server){
+    write_fd.open(cmd_fifo_path);
+    read_fd.open(status_fifo_path);
+    this->server = server;
+    if(!write_fd.is_open()){
+        std::cerr << "FATAL: Failed to open fifo to send wake to the server.\n";
+        server->stop();
+        return;
+    }
+    serverStatus = UNKNOWN;
+    playercount = -1;
+    players = new std::list<std::string>();
 
+    if(read_fd.is_open()){
+        reader = std::thread([this](){ 
+            while(read_fd.good())
+                refresh();
+            this->server->stop();
+        });
+    }else {
+        std::cerr << "NON-FATAL: Failed to open fifo to get status from server.\n";
+    }
 }
 
 idleDaemonIPC::~idleDaemonIPC(){
-
-}
-
-bool idleDaemonIPC::bad() const {
-    
+    if(reader.joinable())
+        reader.join();
+    write_fd.close();
+    read_fd.close();
+    delete players;
 }
 
 void idleDaemonIPC::refresh(){
-
+    std::string in;
+    getline(read_fd, in);
+    int space = in.find(' ');
+    if(space == std::string::npos){
+        std::cerr << "Each line must have at least 2 words separated by a space.\n";
+        return;
+    }
+    std::string word1 = in.substr(0, space);
+    std::string word2 = in.substr(space+1,in.length());
+    if(word1 == "STATUS"){
+        if(word2 == "OFFLINE")
+            serverStatus = OFFLINE;
+        else if(word2 == "ONLINE")
+            serverStatus = ONLINE;
+        else if(word2 == "STARTING")
+            serverStatus = STARTING;
+        else if(word2 == "STOPPING")
+            serverStatus = STOPPING;
+        else{
+            std::cerr << "STATUS value is not OFFLINE|ONLINE|STARTING|STOPPING\n";
+            serverStatus = UNKNOWN;
+        }
+    }else if(word1 == "PLAYERS"){
+        std::list<std::string>* playersInfo = new std::list<std::string>();
+        for(int it = 0; space != std::string::npos; it = space+1){
+            space = word2.substr(it, word2.length()).find(' ');
+            playersInfo->push_back(word2.substr(it, (space == std::string::npos) ? word2.length() : space));
+        }
+        if(playersInfo->size() < 2){
+            std::cerr << "PLAYERS must have at least 2 following words\n";
+            return;
+        }
+        playercount = std::stoi(playersInfo->front());
+        playersInfo->pop_front();
+        maxPlayercount = std::stoi(playersInfo->front());
+        playersInfo->pop_front();
+        delete players;
+        players = playersInfo;
+    }
 }
 
 std::string idleDaemonIPC::getSeverStatus() const {
-
+    switch(status){
+        case OFFLINE:
+            return "OFFLINE";
+        case ONLINE:
+            return "ONLINE";
+        case STARTING:
+            return "STARTING";
+        case STOPPING:
+            return "STOPPING";
+        default:
+            return "UNKNOWN";
+    }
 }
 
 int idleDaemonIPC::getPlayercount() const {
-
+    return playercount;
 }
 
 void idleDaemonIPC::wake(){
-    
+
 }
--- web-wake/src/idleDaemonIPC.hpp
+++ web-wake/src/idleDaemonIPC.hpp
@@ -1,22 +1,31 @@
 #ifndef IDLE_DAEMON_IPC_HPP
 #define IDLE_DAEMON_IPC_HPP
 
+#include <atomic>
+#include <fstream>
 #include <string>
+#include <thread>
+#include "../include/httplib.h"
+
+enum status {OFFLINE, ONLINE, STARTING, STOPPING, UNKNOWN};
 
 class idleDaemonIPC{
 private:
-    int write_fd;//fd of fifo to _send_ commands to the idle daemon
-    int read_fd;//fd of fifo to _receive_ updates from the idle daemon
-    bool connected;
+    std::ofstream write_fd;//fd of fifo to _send_ commands to the idle daemon
+    std::ifstream read_fd;//fd of fifo to _receive_ updates from the idle daemon
+    httplib::Server* server;
 
-    std::string serverStatus;
-    int playercount;
+    std::atomic<status> serverStatus;
+    std::atomic<int> playercount;
+    std::atomic<int> maxPlayercount;
+    std::atomic<std::list<std::string>*> players;
+
+    void refresh();//blocking refresh from read_fd
+    std::thread reader;
 public:
-    idleDaemonIPC(const char* cmd_fifo_path, const char* status_fifo_path);
+    idleDaemonIPC(const char* cmd_fifo_path, const char* status_fifo_path, httplib::Server* server);
     ~idleDaemonIPC();
 
-    bool bad() const;
-    void refresh();
     std::string getSeverStatus() const;
     int getPlayercount() const;
 
