1587dc8fac83ab415081b3120f6a1115c68c5acc
CreeperHappy1 <jorgejuan.azpeitia.pozo@gmail.com>
2026-08-02 02:05:46 +0200

    added debugging line

--- web-wake/src/main.cpp
+++ web-wake/src/main.cpp
@@ -73,5 +73,6 @@ int main(int argc, char* argv[]){
 
     httpsServer server(config["listen host"], config["listen port"], config["token"], argv[1], argv[2]);
     
+    std::cout << "Starting https server...\n";
     return server.run();
 }
