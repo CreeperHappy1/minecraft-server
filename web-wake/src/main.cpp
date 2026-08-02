7daab8a1b5605d73518c4f2677d51cd89bf846c9
CreeperHappy1 <jorgejuan.azpeitia.pozo@gmail.com>
2026-08-02 17:35:42 +0200

    updated token storage to better fit unix standards

--- web-wake/src/main.cpp
+++ web-wake/src/main.cpp
@@ -1,21 +1,31 @@
 #include "httpsServer.hpp"
+#include <fstream>
 #include <iomanip>
+#include <ios>
+#include <iostream>
 #include <sstream>
 #include <json.hpp>
+#include <string>
 #include <sys/random.h>
+#include <filesystem>
+#include <system_error>
 using json = nlohmann::json;
 
-//in case config.json is missing it will be regenerated
+//in case config.json is missing these will be used
 const json defaultConfig = {
     {"listen host", "0.0.0.0"},
-    {"listen port", 25563},
-    {"token", "uninitialized"}
+    {"listen port", 25563}
 };
 
 #ifdef CONFIG_PATH
 const std::string configPath = CONFIG_PATH;
 #else
 const std::string configPath = "config.json";//fallback
+#endif
+#ifdef TOKEN_PATH
+const std::string tokenPath = TOKEN_PATH;
+#else
+const std::string tokenPath = ".web-wake-token";//fallback
 #endif
 
 std::string genToken(){
@@ -36,6 +46,45 @@ std::string genToken(){
     return ss.str();
 }
 
+void writeToken(std::string newToken){
+    std::ofstream ofs(tokenPath, std::ios_base::out | std::ios_base::trunc);
+    if(ofs.is_open()){
+        ofs << newToken << std::endl;
+        ofs.close();
+        std::error_code ec;
+        std::filesystem::permissions(tokenPath, std::filesystem::perms::owner_read | std::filesystem::perms::owner_write,
+            std::filesystem::perm_options::replace, ec);
+        if(ec)
+            std::cerr << "WARNING: Failed to set 600 permissions for token file: " << ec.message() << std::endl;
+    }else
+        std::cerr << "WARNING: Failed to open " << tokenPath << " for writing, using temporary token for this run:\n" << newToken << std::endl;
+}
+
+std::string getOrGenToken(){
+    std::string token;
+    if(!std::filesystem::exists(tokenPath)){//if it doesn't exist, generate it
+        std::cerr << "Token file does not exists, generating...\n";
+        token = genToken();
+        writeToken(token);
+    }else{//if it does exist, try to open it
+        std::ifstream ifs(tokenPath);
+        if(ifs.is_open()){
+            std::getline(ifs, token);
+            ifs.close();
+            if(token.empty()){
+                std::cerr << "ERROR: Failed to get token from " << tokenPath << ", attempting to regenerate...\n";
+                token = genToken();
+                writeToken(token);
+            }
+        }else{
+            std::cerr << "ERROR: Token file exists but is unreadable, attempting to overwrite...\n";
+            token = genToken();
+            writeToken(token);
+        }
+    }
+    return token;
+}
+
 int main(int argc, char* argv[]){
     if(argc < 3){
         std::string thisname = (argv[0] == nullptr || strcmp(argv[0], "") == 0) ? "<this executable>" : argv[0];
@@ -45,33 +94,15 @@ int main(int argc, char* argv[]){
     std::ifstream configFile(configPath);
     json config;
     if(!configFile){
-        std::ofstream ofs(configPath);
-        if(ofs.is_open()){
-            std::cerr << "WARNING: No " << configPath << " was found, regenerating from defaults.\n";
-            ofs << defaultConfig.dump(4) << '\n';
-            ofs.close();
-        }else 
-            std::cerr << "ERROR: No config.json was found and it couldn't be regenerated, using defaults.\n";
+        std::cerr << "ERROR: No config.json was found, using defaults.\n";
         config = defaultConfig;
     }else{
         config = json::parse(configFile);
     }
-    if(config["token"] == "uninitialized"){
-        try{
-            std::ofstream ofs(configPath, std::fstream::out | std::fstream::trunc);
-            if(ofs.is_open()){
-                config["token"] = genToken();
-                ofs << defaultConfig.dump(4) << '\n';
-                ofs.close();
-            }else
-                std::cerr << "WARNING: Failed to open " << configPath << " to write the generated token, it will remain as \"uninitialized\".\n";
-        }catch(const std::exception &e){
-            std::cerr << "WARNING: Failed to generate and write token, left uninitialized.\n";
-            std::cerr << "\tCaught exception: " << e.what() << std::endl;
-        }
-    }
+    
+    std::string token = getOrGenToken();
 
-    httpsServer server(config["listen host"], config["listen port"], config["token"], argv[1], argv[2]);
+    httpsServer server(config["listen host"], config["listen port"], token, argv[1], argv[2]);
     
     std::cout << "Starting https server...\n";
     return server.run();
